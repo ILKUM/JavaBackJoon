33fab1365c4a81a2f1885bcdb27312b282f7b9f1
ILGYEOM <chdl1229@gmail.com>
2020-08-09 17:58:24 +0900

    백준 1008번

---
+++ BackJoonStudy/src/Main3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main3 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		double a = s.nextDouble();
+		double b = s.nextDouble();
+		System.out.println(a/b);
+		s.close();
+		}
+}
